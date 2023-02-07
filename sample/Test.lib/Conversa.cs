cc25324845fba27e3da15a4890921e6bd365f64f
Leonardo Vinivius De Gasperin <leonardovdg@hotmail.com>
2023-02-07 19:39:13 -0300

    add ex 4 && 5

--- sample/Test.lib/Conversa.cs
+++ sample/Test.lib/Conversa.cs
@@ -1,13 +1,22 @@
 ﻿namespace Test.lib;
 public class Conversa
 {
-    public string Falar(int i)
+    private readonly string[] randonTalk = { "papagaio", "sábado", "quem sabe amanhã", "não" };
+
+    public static string Falar(int i)
     {
         if(i % 2 == 0)
         {
             return "Oi";
         }
         return "Tchau";
+    }
+
+    public string FalaAleatoria()
+    {
+        Random rnd= new();
+
+        return randonTalk[rnd.Next(0, randonTalk.Length)];
     }
 
 }
--- sample/lib/UnitTest1.cs
+++ sample/lib/UnitTest1.cs
@@ -12,13 +12,12 @@ public class Tests
     [Test]
     public void Test1()
     {
-        var t = new Conversa();
-        string aux = string.Empty;
+        string aux;
         int cnt = 0;
         for(int i = 0; i < 1000; i++)
         {
-            aux = t.Falar(i);
-            if (i % 2 == 0 && aux != "Oi")
+            aux = Conversa.Falar(i);
+            if(i % 2 == 0 && aux != "Oi")
                 cnt++;
             if(i % 2 != 0 && aux != "Tchau")
                 cnt++;
@@ -26,4 +25,25 @@ public class Tests
         Assert.That(cnt, Is.Zero);
     }
 
+    [Test]
+    public void Test2()
+    {
+        var t = new Conversa();
+        int maxValueWithouRepeat = 4;
+        List<string> aux = new()
+        {
+            t.FalaAleatoria()
+        };
+        for (int i = 0; i < 1000; i++)
+        {
+            var v = t.FalaAleatoria();
+            if(!aux.Contains(v))
+            {
+                aux.Add(v);
+            }
+            if (aux.Count == 4)
+                break;
+        }
+        Assert.AreEqual(aux.Count, 4);
+    }
 }
