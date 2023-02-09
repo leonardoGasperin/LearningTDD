ffe0db7c7a3799514666701b974f63251670f3d3
Leonardo Vinicius De Gasperin <leonardovdg@hotmail.com>
2023-02-09 14:00:55 -0300

    Commit Test

---
+++ sample/Test.lib/Contato.cs
@@ -0,0 +1,11 @@
+﻿namespace Test.lib.Models
+{
+    public class Contato
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Telefone { get; set; }
+
+    }
+
+}
--- sample/Test.lib/Conversa.cs
+++ sample/Test.lib/Conversa.cs
@@ -1,11 +1,14 @@
-﻿namespace Test.lib;
+﻿using Test.lib.Models;
+
+namespace Test.lib;
 public class Conversa
 {
     private readonly string[] randonTalk = { "papagaio", "sábado", "quem sabe amanhã", "não" };
+    private List<Contato> ContatoLista { get; set; }
 
     public static string Falar(int i)
     {
-        if(i % 2 == 0)
+        if (i % 2 == 0)
         {
             return "Oi";
         }
@@ -14,9 +17,26 @@ public class Conversa
 
     public string FalaAleatoria()
     {
-        Random rnd= new();
+        Random rnd = new();
 
         return randonTalk[rnd.Next(0, randonTalk.Length)];
     }
 
+    public List<Contato> GetAllContato()
+    {
+        if (ContatoLista is null)
+            throw new Exception("Lista de contato vazia");
+        return ContatoLista;
+    }
+
+    public bool AddContato(Contato add)
+    {
+        if (add.Name.Count() is 0)
+            throw new Exception("Nome Invalido");
+        if (!long.TryParse(add.Telefone, out long n) || add.Telefone.Count() is < 9)
+            throw new Exception("Telefone Invalido");
+        ContatoLista.Add(add);
+
+        return true;
+    }
 }
